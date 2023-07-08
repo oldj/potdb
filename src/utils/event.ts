098df9834c765ef5b4e0e4fc79def36b1656e2ee
oldj <oldj.wu@gmail.com>
2023-07-09 00:56:18 +0800

    调整事件函数结构。

--- src/utils/event.ts
+++ src/utils/event.ts
@@ -6,12 +6,72 @@
 
 import lodash from 'lodash'
 import { DataObjectType } from '@/types/basic'
-import { DataActionType } from '@/types/event'
+import { DataActionType, DataType } from '@/types/event'
 import Collection from '@core/datatype/collection'
 
 type GetValueFunc = (obj: DataObjectType, ...args: any[]) => any
 type GetValueType = GetValueFunc | 'all' | 'filter' | 'result'
 type GetOriginalValueType = 'get' | 'all' | 'filter'
+
+interface ICheckIsNotChangedOptions {
+  type: DataType
+  action: DataActionType
+  has_original_value: boolean
+  original_value: any
+  method_name: string
+  args: any[]
+}
+
+function checkIsNotChanged(options: ICheckIsNotChangedOptions): boolean {
+  // 检查传入的值是否与原值相同
+  const { type, action, has_original_value, original_value, method_name, args } = options
+
+  let is_not_changed = false
+  if (action === 'update' && has_original_value) {
+    if (type === 'collection' && method_name === 'update') {
+      if (original_value && Array.isArray(original_value) && original_value.length === 1) {
+        let item = original_value[0]
+        let update = args[1]
+        let keys = Object.keys(update)
+        if (lodash.isEqual(lodash.pick(item, keys), update)) {
+          is_not_changed = true
+        }
+      }
+    } else if (type === 'dict') {
+      if (method_name === 'set') {
+        let value = args[1]
+        if (original_value === value) {
+          is_not_changed = true
+        }
+      } else if (method_name === 'update') {
+        let obj = args[0]
+        let keys = Object.keys(obj)
+        if (original_value && lodash.isEqual(lodash.pick(original_value, keys), obj)) {
+          is_not_changed = true
+        }
+      }
+    } else if (type === 'list') {
+      if (method_name === 'set' || method_name === 'update') {
+        let new_list = args[0]
+        if (lodash.isEqual(original_value, new_list)) {
+          is_not_changed = true
+        }
+      }
+    } else if (type === 'set') {
+      if (method_name === 'set' || method_name === 'update') {
+        let new_set_arr = args[0]
+        if (
+          original_value &&
+          lodash.isEqual(original_value.slice().sort(), new_set_arr.slice().sort())
+        ) {
+          is_not_changed = true
+        }
+      }
+    }
+  }
+
+  return is_not_changed
+}
 
 export const listen = (
   action: DataActionType,
@@ -50,53 +110,14 @@ export const listen = (
           }
         }
       }
-      let is_not_changed = false
-      if (action === 'update' && has_original_value) {
-        // 检查传入的值是否与原值相同
-        let type = this.type
-        let method_name = method.name
-
-        if (type === 'collection' && method_name === 'update') {
-          if (original_value && Array.isArray(original_value) && original_value.length === 1) {
-            let item = original_value[0]
-            let update = args[1]
-            let keys = Object.keys(update)
-            if (lodash.isEqual(lodash.pick(item, keys), update)) {
-              is_not_changed = true
-            }
-          }
-        } else if (type === 'dict') {
-          if (method_name === 'set') {
-            let value = args[1]
-            if (original_value === value) {
-              is_not_changed = true
-            }
-          } else if (method_name === 'update') {
-            let obj = args[0]
-            let keys = Object.keys(obj)
-            if (original_value && lodash.isEqual(lodash.pick(original_value, keys), obj)) {
-              is_not_changed = true
-            }
-          }
-        } else if (type === 'list') {
-          if (method_name === 'set' || method_name === 'update') {
-            let new_list = args[0]
-            if (lodash.isEqual(original_value, new_list)) {
-              is_not_changed = true
-            }
-          }
-        } else if (type === 'set') {
-          if (method_name === 'set' || method_name === 'update') {
-            let new_set_arr = args[0]
-            if (
-              original_value &&
-              lodash.isEqual(original_value.slice().sort(), new_set_arr.slice().sort())
-            ) {
-              is_not_changed = true
-            }
-          }
-        }
-      }
+      let is_not_changed = checkIsNotChanged({
+        type: this.type,
+        action,
+        has_original_value,
+        original_value,
+        method_name: method.name,
+        args: new_args,
+      })
 
       // 执行原方法
       let result = await method.apply(this, new_args)
